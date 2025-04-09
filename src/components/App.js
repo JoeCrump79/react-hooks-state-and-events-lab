c2ea8e1fcdd73e0bc7f9431352bd490d98830145
Joseph Crump <josephcrump79@yahoo.com>
2025-04-09 13:07:08 -0500

    react-hooks-state-and-events-lab completed

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import ShoppingList from "./ShoppingList";
 import itemData from "../data/items";
 
 function App() {
+  // Step 1: set up state
+  const [isDarkMode, setIsDarkMode] = useState(false);
 
-  // replace 'false' with a state variable that can be toggled between true and false
-  // this will be used for the Dark Mode Toggle feature
-  const appClass = false ? "App dark" : "App light"
+  // Step 2: determine class based on state
+  const appClass = isDarkMode ? "App dark" : "App light";
+
+  // Step 3: toggle function
+  function handleClick() {
+    setIsDarkMode((isDarkMode) => !isDarkMode);
+  }
 
   return (
     <div className={appClass}>
       <header>
         <h2>Shopster</h2>
-        <button>Dark Mode</button>
+        <button onClick={handleClick}>
+          {isDarkMode ? "Light Mode" : "Dark Mode"}
+        </button>
       </header>
       <ShoppingList items={itemData} />
     </div>
